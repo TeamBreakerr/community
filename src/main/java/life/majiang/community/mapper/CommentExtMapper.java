7b46059b6ceee9dfce0b2ea8a42d2a69931e3aab
TeamBreaker <799839863@qq.com>
2022-08-19 22:54:01 +0800

    完成二级回复页面绘制，回复数显示

---
+++ src/main/java/life/majiang/community/mapper/CommentExtMapper.java
@@ -0,0 +1,6 @@
+package life.majiang.community.mapper;
+
+public interface CommentExtMapper {
+    //评论数自增的sql映射
+    int incCommentCount(Integer id);
+}
--- src/main/java/life/majiang/community/service/CommentService.java
+++ src/main/java/life/majiang/community/service/CommentService.java
@@ -4,10 +4,7 @@ import life.majiang.community.dto.CommentDTO;
 import life.majiang.community.enums.CommentTypeEnum;
 import life.majiang.community.exception.CustomizeErrorCode;
 import life.majiang.community.exception.CustomizeException;
-import life.majiang.community.mapper.CommentMapper;
-import life.majiang.community.mapper.QuestionExtMapper;
-import life.majiang.community.mapper.QuestionMapper;
-import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.mapper.*;
 import life.majiang.community.model.*;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,6 +30,9 @@ public class CommentService {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private CommentExtMapper commentExtMapper;
 
     @Transactional
     public void insert(Comment comment) {
@@ -63,6 +63,8 @@ public class CommentService {
                 throw new CustomizeException(CustomizeErrorCode.COMMENT_NOT_FOUND);
             }
             commentMapper.insertSelective(comment);
+            //回复数加一
+            commentExtMapper.incCommentCount(comment.getParentId());
         }
     }
 
